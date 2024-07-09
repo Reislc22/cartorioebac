83ffb93c77195d8a31a2eec452ca9d4fd680bc73
Reislc22 <reislc22@gmail.com>
2024-07-09 12:04:41 -0300

    Adicionado a opção de saida

--- Ebac_sla.c
+++ Ebac_sla.c
@@ -159,7 +159,8 @@ int main()
     	printf("Escolher a opção desejada do menu:\n\n");
 	    printf("\t1- Registras nomes\n");
 	    printf("\t2- Consultar nomes\n");
-	    printf("\t3- Deletar nomes\n\n");
+	    printf("\t3- Deletar nomes\n");
+	    printf("\t4- Sair do progama\n\n");
 	    printf("opção: ");
     //Final do menu
 	
@@ -184,6 +185,12 @@ int main()
 			case 3:	
 			deletar();
    	        break;
+   	        
+   	        case 4:
+   	        printf("Obrigado por utilizar o sistem\n");
+   	        return 0;
+   	        break;
+   	        
    	//Acabou de chamar as funções
    	
    	        default:
